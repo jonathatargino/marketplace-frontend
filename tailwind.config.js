eab0b6dff9ebde7a53f32d6e56304db76290237b
Jonatha Targino <j.oliveiratargino@hotmail.com>
2024-03-17 09:10:02 -0300

    structure(MF002): add screens and palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,29 @@
-/ @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src//*.{js,ts,jsx,tsx}",
-  ],
+/ @type {import('tailwindcss').Config} */;
+module.exports = { //eslint-disable-line
+  content: ["./index.html", "./src//*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "744px",
+      md: "1024px",
+      lg: "1440px",
+    },
     extend: {
       colors: {
         primary: {
-          light: "#",
-          main: "#",
-          dark: "#",
+          main: "#244323",
+          dark: "#1A301A",
         },
         secondary: {
-          light: "#",
-          main: "#",
-          dark: "#",
+          main: "#6256A6",
+          dark: "#473E77",
         },
         success: {
-          light: "#86FF98",
-          main: "#5FD967",
-          dark: "#3E8C43",
+          main: "#22c55e",
+          dark: "#15803d",
         },
         error: {
-          light: "#E57373",
-          main: "#F44336",
-          dark: "#D32F2F",
+          main: "#ef4444",
+          dark: "#b91c1c",
         },
       },
     },
